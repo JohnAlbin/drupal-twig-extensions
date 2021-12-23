abfea67dc0fcc9e91eaf383170dea269aeff9003
JohnAlbin <virtually.johnalbin@gmail.com>
2021-12-23 10:28:22 -0600

    test: Add tests for create_attribute function
    Issue #6

---
+++ tests/Twig.js/functions/create_attribute.js
@@ -0,0 +1,70 @@
+import test from 'ava';
+import { setupTwigBefore, renderTemplateMacro } from '#twig-fixture';
+
+test.before(setupTwigBefore);
+
+test(
+  'should render to an empty string with no parameters',
+  renderTemplateMacro,
+  {
+    template: '<div{{ create_attribute() }}>',
+    data: {},
+    expected: '<div>',
+  },
+);
+
+test.failing(
+  'should create an Attribute object with static parameters',
+  renderTemplateMacro,
+  {
+    template:
+      '<div{{ create_attribute({ id: "example", class: ["class1", "class2"] }) }}>',
+    data: {},
+    expected: '<div id="example" class="class1 class2">',
+  },
+);
+
+test(
+  'should create an Attribute object with object variable parameters',
+  renderTemplateMacro,
+  {
+    template: '<div{{ create_attribute(attributes) }}>',
+    data: {
+      attributes: { id: 'example', class: ['class1', 'class2'] },
+    },
+    expected: '<div id="example" class="class1 class2">',
+  },
+);
+
+test(
+  'should create an Attribute object with map variable parameters',
+  renderTemplateMacro,
+  {
+    template: '<div{{ create_attribute(attributes) }}>',
+    data: {
+      attributes: new Map([
+        ['id', 'example'],
+        ['class', ['class1', 'class2']],
+      ]),
+    },
+    expected: '<div id="example" class="class1 class2">',
+  },
+);
+
+test('should return an Attribute object with methods', renderTemplateMacro, {
+  template:
+    '<div{{ create_attribute().setAttribute("id", "example").addClass("class1", "class2") }}>',
+  data: {},
+  expected: '<div id="example" class="class1 class2">',
+});
+
+test.failing(
+  'should return an Attribute object with accessible properties',
+  renderTemplateMacro,
+  {
+    template:
+      '{% set attributes = create_attribute({ "id": "example" }) %}id:{{ attributes.id }}:',
+    data: {},
+    expected: 'id:example:',
+  },
+);
