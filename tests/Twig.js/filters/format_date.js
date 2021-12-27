eaa059b41bd61ad038e37b6e0b04a7398df293e1
JohnAlbin <virtually.johnalbin@gmail.com>
2021-12-27 00:14:42 -0600

    feat(format_date): Add format_date filter
    Fixes #10

--- tests/Twig.js/filters/format_date.js
+++ tests/Twig.js/filters/format_date.js
@@ -4,15 +4,38 @@ import { setupTwigBefore, renderTemplateMacro } from '#twig-fixture';
 
 test.before(setupTwigBefore);
 
-const format = 'Y-m-d H:i:s T';
-const template = `{{ date|format_date("${format}") }}`;
-
 const data = {
   date: new Date(Date.UTC(2002, 5, 29, 10, 0, 0)),
 };
 
-test('should format the given Date', renderTemplateMacro, {
-  template,
+test('should use the "medium" date type by default', renderTemplateMacro, {
+  template: `{{ date|format_date }}`,
   data,
+  expected: date('D, m/d/Y - H:i', data.date),
+});
+
+test('should use the given date type', renderTemplateMacro, {
+  template: `{{ date|format_date("long") }}`,
+  data,
+  expected: date('l, F j, Y - H:i', data.date),
+});
+
+test(
+  'should use the "Fallback date format" if the given date type is not found',
+  renderTemplateMacro,
+  {
+    template: `{{ date|format_date("notfound") }}`,
+    data,
+    expected: date('D, m/d/Y - H:i', data.date),
+  },
+);
+
+const format = 'Y-m-d H:i:s T';
+test('should use the given custom format', renderTemplateMacro, {
+  template: `{{ date|format_date("custom", format) }}`,
+  data: {
+    ...data,
+    format,
+  },
   expected: date(format, data.date),
 });
