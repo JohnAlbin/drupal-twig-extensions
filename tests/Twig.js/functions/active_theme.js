5ab8bdf3913ba406b89ea581f48a7adaf98e94b0
John Albin Wilkins <virtually.johnalbin@gmail.com>
2021-12-30 01:28:51 +0800

    feat: Add setup options for active_theme (#34)
    Fixes #32

--- tests/Twig.js/functions/active_theme.js
+++ tests/Twig.js/functions/active_theme.js
@@ -1,14 +1,45 @@
 import test from 'ava';
+import Twig from 'twig';
+import state from '../../../lib/config/twig.js';
+import { addDrupalExtensions } from '#twig';
 import { setupTwigBefore, renderTemplateMacro } from '#twig-fixture';
 
 test.before(setupTwigBefore);
 
-test('should render with no arguments', renderTemplateMacro, {
-  template: 'This is my {{ active_theme() }}!',
-  expected: 'This is my !',
+test.serial('should render with no arguments', renderTemplateMacro, {
+  template: 'This is the {{ active_theme() }} theme!',
+  expected: 'This is the stark theme!',
 });
 
-test('should render while ignoring arguments', renderTemplateMacro, {
-  template: 'This is my {{ active_theme("ignored argument") }}!',
-  expected: 'This is my !',
+test.serial('should render while ignoring arguments', renderTemplateMacro, {
+  template: 'This is the {{ active_theme("ignored argument") }} theme!',
+  expected: 'This is the stark theme!',
+});
+
+test.serial('should use the theme added from config', async (t) => {
+  const activeTheme = 'myCustomTheme';
+  const originalTheme = state.activeTheme;
+
+  // Ensure the config is reverted for other tests.
+  t.teardown(() => {
+    state.activeTheme = originalTheme;
+  });
+
+  t.plan(3);
+
+  // Confirm that the original config is different.
+  t.not(state.activeTheme, activeTheme);
+
+  addDrupalExtensions(Twig, {
+    active_theme: activeTheme,
+  });
+
+  // Confirm that the config was added to the state.
+  t.deepEqual(state.activeTheme, activeTheme);
+
+  // Confirm the config affects rendering.
+  await renderTemplateMacro.exec(t, {
+    template: '{{ active_theme() }}',
+    expected: activeTheme,
+  });
 });
--- tests/Twing/functions/active_theme.js
+++ tests/Twing/functions/active_theme.js
@@ -1,14 +1,51 @@
 import test from 'ava';
+import { TwingEnvironment, TwingLoaderRelativeFilesystem } from 'twing';
+import state from '../../../lib/config/twing.js';
+import { addDrupalExtensions } from '#twing';
 import { setupTwingBefore, renderTemplateMacro } from '#twing-fixture';
 
 test.before(setupTwingBefore);
 
-test('should render with no arguments', renderTemplateMacro, {
-  template: 'My theme is {{ active_theme() }}!',
-  expected: 'My theme is !',
+test.serial('should render with no arguments', renderTemplateMacro, {
+  template: 'This is the {{ active_theme() }} theme!',
+  expected: 'This is the stark theme!',
 });
 
-test('should render while ignoring arguments', renderTemplateMacro, {
-  template: 'My theme is {{ active_theme("ignored argument") }}!',
-  expected: 'My theme is !',
+test.serial('should render while ignoring arguments', renderTemplateMacro, {
+  template: 'This is the {{ active_theme("ignored argument") }} theme!',
+  expected: 'This is the stark theme!',
+});
+
+test.serial('should use the theme added from config', async (t) => {
+  const activeTheme = 'myCustomTheme';
+  const originalTheme = state.activeTheme;
+
+  // Ensure the config is reverted for other tests.
+  t.teardown(() => {
+    state.activeTheme = originalTheme;
+  });
+
+  t.plan(3);
+
+  // Create an instance of the Twing Environment.
+  const twingEnvironment = new TwingEnvironment(
+    new TwingLoaderRelativeFilesystem(),
+    { autoescape: false },
+  );
+
+  // Confirm that the original config is different.
+  t.not(state.activeTheme, activeTheme);
+
+  addDrupalExtensions(twingEnvironment, {
+    active_theme: activeTheme,
+  });
+
+  // Confirm that the config was added to the state.
+  t.deepEqual(state.activeTheme, activeTheme);
+
+  // Confirm the config affects rendering.
+  await renderTemplateMacro.exec(t, {
+    template: '{{ active_theme() }}',
+    expected: activeTheme,
+  });
 });
