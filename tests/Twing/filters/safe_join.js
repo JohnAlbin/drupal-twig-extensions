ea2531e9d9deb3b6f9fa8fca786707c791aa0491
JohnAlbin <virtually.johnalbin@gmail.com>
2022-01-01 10:26:02 -0600

    fix(Twing): safe_join filter in Twing should join object values
    Fixes #44

--- tests/Twing/filters/safe_join.js
+++ tests/Twing/filters/safe_join.js
@@ -21,18 +21,14 @@ test('should join an array with the given glue string', renderTemplateMacro, {
   expected: 'string1, string2, string3',
 });
 
-test.failing(
-  'should join an object with the given glue string',
-  renderTemplateMacro,
-  {
-    template: '{{ object|safe_join(", ") }}',
-    data: {
-      object: {
-        key1: 'string1',
-        key2: 'string2',
-        key3: 'string3',
-      },
+test('should join an object with the given glue string', renderTemplateMacro, {
+  template: '{{ object|safe_join(", ") }}',
+  data: {
+    object: {
+      key1: 'string1',
+      key2: 'string2',
+      key3: 'string3',
     },
-    expected: 'string1, string2, string3',
   },
-);
+  expected: 'string1, string2, string3',
+});
