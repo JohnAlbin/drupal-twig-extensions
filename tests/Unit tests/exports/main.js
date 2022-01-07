2946333920f73d2ea976800aa8644c143e65a936
JohnAlbin <virtually.johnalbin@gmail.com>
2022-01-07 10:47:53 -0600

    test: Update code comments in exports tests

--- tests/Unit tests/exports/main.js
+++ tests/Unit tests/exports/main.js
@@ -3,7 +3,7 @@ import DrupalAttribute from 'drupal-attribute';
 import * as exports from '../../../index.cjs';
 
 test('should have 1 named export', (t) => {
-  // CJS files also include "default" and "__esModule" exports.
+  // Babel-transpiled CJS files also include "default" and "__esModule" exports.
   t.is(Object.keys(exports).length - 2, 1);
 });
 
--- tests/Unit tests/exports/twig.js
+++ tests/Unit tests/exports/twig.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import * as exports from '../../../twig.cjs';
 
 test('should have 2 named exports', (t) => {
-  // CJS files also include "default" and "__esModule" exports.
+  // Babel-transpiled CJS files also include "default" and "__esModule" exports.
   t.is(Object.keys(exports).length - 2, 2);
 });
 
